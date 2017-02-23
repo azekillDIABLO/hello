7b821f542de3138582d02990b2c55c9528ba2ff4
azekillDIABLO <azekill.diablo@gmail.com>
2017-02-23 19:17:43 +0100

    correct a small bug...

--- init.lua
+++ init.lua
@@ -12,12 +12,11 @@ minetest.register_chatcommand("hello", {
 	privs = {},
 	description = "Shows a list of images.",
 	func = function(name, player)
-	local name = player:get_player_name()
 	-- Define the formspec
 	minetest.show_formspec(name, "hello:form",
 		"size[26,14]" ..
 		"background[0,0;4.5,3.5;hello_background_" .. page .. ".png;true]" ..
-		"label[0,0;Welcome to Hello Image Viewer," .. name .. ". Type /hello to see this window if you closed it!]" ..
+		"label[0,0;Welcome to Hello Image Viewer. Type /hello to see this window if you closed it!]" ..
 		"button[0.5,13.5;2,1;next_button;next ->]" ..
 		"button_exit[19,13.5;7,0.5;exit;Click here to close this beautiful formspec...]")
 	end
