05153c14269ec86c26ffd413234392d730cdc2a4
azekillDIABLO <Rodrigue.Lemaire@gmail.com>
2017-02-23 19:09:06 +0100

    Init repository

---
+++ description.txt
@@ -0,0 +1,10 @@
+Here put file called :
+"hello_background_1" then
+"hello_background_2" then
+"hello_background_3" etc...
+
+When you finished, count the number of files,
+then change, in init.lua, "max_page = <number>"
+to something like "max_page = 15".
+
+Then it's ok you can use it by typing /hello
---
+++ init.lua
@@ -0,0 +1,40 @@
+-- Welcome to Hello Ingame Image Viewer
+hello = {}
+
+-- First some global variables
+page = 1
+name = player
+max_page = 7 --<<<<<<here change!
+
+-- Show form when player types /hello.
+minetest.register_chatcommand("hello", {
+	params = "",
+	privs = {},
+	description = "Shows a list of images.",
+	func = function(name, player)
+	local name = player:get_player_name()
+	-- Define the formspec
+	minetest.show_formspec(name, "hello:form",
+		"size[26,14]" ..
+		"background[0,0;4.5,3.5;hello_background_" .. page .. ".png;true]" ..
+		"label[0,0;Welcome to Hello Image Viewer," .. name .. ". Type /hello to see this window if you closed it!]" ..
+		"button[0.5,13.5;2,1;next_button;next ->]" ..
+		"button_exit[19,13.5;7,0.5;exit;Click here to close this beautiful formspec...]")
+	end
+})
+
+-- The next button is pressed
+minetest.register_on_player_receive_fields(function(player, formname, fields)
+	local name = player:get_player_name()
+	page = page + 1
+	minetest.show_formspec(name, "hello:form",
+		"size[26,14]" ..
+		"background[0,0;4.5,3.5;hello_background_" .. page .. ".png;true]" ..
+		"label[0,0;Welcome to Hello Image Viewer," .. name .. ". Type /hello to see this window if you closed it!]" ..
+		"button[0.5,13.5;2,1;next_button;next ->]" ..
+		"button_exit[19,13.5;7,0.5;exit;Click here to close this beautiful formspec...]")
+	minetest.debug("Hello:Changed page... Now on page:" .. page .." of " .. max_page .. ".")
+	if page == max_page then
+		page = 0
+	end
+end)
